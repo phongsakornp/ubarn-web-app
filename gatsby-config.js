595e6a4e0552b3f4839fbbc8609486cb977bf60b
PhongsakornP <i.phongsakornp@gmail.com>
2020-04-03 08:11:39 +0700

    feat: add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,12 @@ module.exports = {
       },
     },
     `gatsby-transformer-json`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-148363023-3',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
